09289d267bcd0b78988abd63ce24149d285fc0b7
unknown <acebalao@mail.uc.edu>
2022-09-18 22:49:09 -0400

    Lab 3

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,6 @@
+﻿$IP = getIP
+$User = getUser
+$Hostname = getHostName
+$PSV = getPSV
+$Date = getDate
+Write-Host("This machine's IP is $IP. User is $User. Hostname is $Hostname. Powershell Version $PSV. Today's date is $Date.")
